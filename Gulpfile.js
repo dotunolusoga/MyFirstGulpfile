3ab52419221ef25b48194dfa38a57698c4d4bcdd
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-03-18 15:14:38 -0500

    renamed the Gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var gulp = require('gulp'),
+    del  = require('del'),
+    sass = require('gulp-sass'),
+    jade = require('gulp-jade'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer-core');
+
+gulp
+  .task('clean', function() {
+  del(['.tmp', 'public']);
+});
+
+gulp.task('sass', function () {
+    gulp
+      .src('app/styles/main.scss')
+      .pipe(sass())
+      .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
+      .pipe(gulp.dest('public/css'));
+});
+
+gulp
+  .task('jade', function() {
+  gulp.src('app/**/*.jade')
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('public'));
+});
